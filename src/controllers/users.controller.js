25dabfe4008347246d8a30ab454d5cdb3ded8f4e
Taynara Pecorario <taynara_rp22@hotmail.com>
2023-04-24 22:26:50 -0300

    :bug: fixing language

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -26,13 +26,13 @@ export async function signIn(req, res) {
     const user = await db.collection('users').findOne({ email });
 
     if (!user) {
-      return res.status(404).send('E-mail não cadastrado.');
+      return res.status(404).send('E-mail not registered');
     }
 
     const isRightPassword = bcrypt.compareSync(password, user.password);
 
     if (!isRightPassword) {
-      return res.status(401).send('Senha incorreta.');
+      return res.status(401).send('Incorrect password');
     }
 
     const token = uuid();
